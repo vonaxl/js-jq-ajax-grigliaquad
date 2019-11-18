3c0a13eaea29c4412c9e338754ad82a1af81900c
vonaxl <vonaxlcorso@gmail.com>
2019-11-18 16:14:06 +0100

    push

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -4,34 +4,37 @@ function generateCells() {
     $(".wrap").append('<div class="box"></div>');
   }
 }
-$(document).ready(function() {
-  //   FIRST AJAAAX TO LOAD ON PAGE
+
+// function ask ajax
+function getRandomNum() {
   $.ajax({
     url: "https://flynn.boolean.careers/exercises/api/random/int",
     method: "GET",
     success: function(dati) {
-      console.log("TCL: dati", dati);
+      console.log(dati);
       elemento.html(dati.response);
     },
     error: function() {}
   });
-  var elemento = $("#numrandom");
+}
+//
+//
+
+var elemento = $("#numrandom");
+$(document).ready(function() {
+  getRandomNum();
+  // click action on a box
   $(this).on("click", ".box", function() {
+    // check if this is already clicked or not
+
     if ($(this).hasClass("clicked")) {
       console.log("already clicked");
     } else {
       $(this).addClass("clicked");
-      //   AJAAAAXX
-      $.ajax({
-        url: "https://flynn.boolean.careers/exercises/api/random/int",
-        method: "GET",
-        success: function(dati) {
-          console.log("TCL: dati", dati);
-          elemento.html(dati.response);
-        },
-        error: function() {}
-      });
-      var x = elemento.html();
+      getRandomNum();
+      //    READ THE VALUE OF X FROM THE HTML AND CHECK WETHER TO MAKE IT YELLOW OR GREEN
+
+      x = elemento.html();
       if (x <= 5) {
         $(this).addClass("green");
       } else {
